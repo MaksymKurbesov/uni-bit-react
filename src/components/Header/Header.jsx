4f67deb13f3285df448e4c70746cc2d54a687755
MaksymKurbesov <bonyklade@gmail.com>
2023-01-02 18:59:55 +0300

    added mobile menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,14 @@
 import Logo from "./../../assets/images/logo.png";
 import style from "./Header.module.css";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const [toggle, setToggle] = useState(false);
+
+  useEffect(() => {
+    console.log(toggle);
+  });
+
   return (
     <header>
       <div className={"container"}>
@@ -18,12 +25,40 @@ const Header = () => {
           <nav className={style["mobile-menu"]}>
             <label className={style["menu-button-container"]}>
               <input className={style["checkbox"]} type="checkbox" />
-              <div className={style["hamburger-lines"]}>
+              <div
+                className={style["hamburger-lines"]}
+                onClick={() => {
+                  setToggle(!toggle);
+                }}
+              >
                 <span className={`${style["line"]} ${style["line1"]}`}></span>
                 <span className={`${style["line"]} ${style["line2"]}`}></span>
                 <span className={`${style["line"]} ${style["line3"]}`}></span>
               </div>
             </label>
+            {/*  ${*/}
+            {/*  style[`${mobileMenuIsShown ? "active" : "no-active"}`]*/}
+            {/*}*/}
+            {/*{toggle ? (*/}
+            <ul
+              className={`${style[`nav-list-mobile`]} ${
+                toggle ? style["active"] : ""
+              }`}
+            >
+              <li>
+                <a href={"/#"}>Company</a>
+              </li>
+              <li>
+                <a href={"/#"}>Services</a>
+              </li>
+              <li>
+                <a href={"/#"}>Portfolio</a>
+              </li>
+              <li>
+                <a href={"/#"}>Contact Us</a>
+              </li>
+            </ul>
+            {/*) : null}*/}
           </nav>
           <nav className={style["desktop-menu"]}>
             <ul className={style["nav-list"]}>
